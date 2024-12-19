15731a1ed1ed35efbc59f670587021056a545a04
Anshul <discover.letsfindit@gmail.com>
2024-12-19 12:05:49 +0530

    working tauri

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,38 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vite.dev/config/
+
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  }
+  },
+
+
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  server: {
+    // Tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: host || false,
+    port: 5173,
+  },
+  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 });
